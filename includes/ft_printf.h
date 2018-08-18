7f8dfdf6b075f3a30ccba8cf3c8907ddc31cd46b
benito ESTEBAN-BODIPO <besteba@c3r1s7.wethinkcode.co.za>
2018-08-18 13:26:56 +0200

    Norminette ft_printf.h file

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,21 +6,21 @@
 /*   By: besteba <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 11:12:41 by besteba           #+#    #+#             */
-/*   Updated: 2018/08/17 13:17:35 by besteba          ###   ########.fr       */
+/*   Updated: 2018/08/18 13:25:47 by besteba          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdlib.h>
-#include <limits.h>
+# include <stdlib.h>
+# include <limits.h>
 # include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <wchar.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <wchar.h>
 
-typedef struct		format
+typedef struct		s_format
 {
 	char			flag;
 	unsigned int	width;
@@ -29,7 +29,6 @@ typedef struct		format
 	char			specifier;
 	int				is_negative;
 }					t_format;
-
 
 int					is_in_str(char *str, int c);
 int					is_digit(int c);
@@ -41,25 +40,27 @@ int					ft_strcmp(char *str1, char *str2);
 char				*ft_strdup(const char *str);
 char				*preppend(char *tmp, char *to_insert);
 
-
-void				ft_printf(const char* format, ...);
+void				ft_printf(const char *format, ...);
 void				handle_format(t_format *form, va_list ap);
 uintmax_t			get_unsigned_num(va_list args, t_format *form);
 intmax_t			get_signed_num(va_list args, t_format *form);
 char				*get_length(const char **str);
 void				handle_length(va_list ap, t_format *form, char **tmp);
-static void 		padding_right(char **tmp, char *from, int width, char c);
+static void			padding_right(char **tmp, char *from, int width, char c);
 static void			padding_left(char **tmp, char *from, int width, char c);
-static void			padding(char **tmp, char *str, int width, char simbol, char c);
+static void			padding(char **tmp, char *str, int width,
+									char simbol, char c);
 char				*pointer(char *pointer, t_format *form, int len);
 void				get_pointer(char **tmp, void *ptr, t_format *form);
 void				handle_precision(t_format *form, char **tmp);
 int					get_base(char c);
-void				add_prefix(char **tmp, char **str, char spec, int is_negative);
+void				add_prefix(char **tmp, char **str, char spec,
+							int is_negative);
 static void			print_char(char c, int specifier);
 static void			print_str(char *str, int specifier);
 void				flags_slector(t_format *form, const char **str);
-void				handle_struct(const char **format, t_format *form, va_list ap);
+void				handle_struct(const char **format, t_format *form,
+							va_list ap);
 void				reset_struct(t_format *form);
 int					ft_wcharlen(wchar_t wchar);
 size_t				ft_wstrlen(wchar_t *ws);
@@ -69,5 +70,6 @@ static int			ft_wchartochar(wchar_t wc);
 char				*ft_wstrtostr(wchar_t *wstr);
 void				add_zero(char **tmp, char *str, t_format *form);
 char				*get_num(const char **str, int *dot);
+void				handle_is_negative(va_list ap, t_format *form, char **tmp);
 
 #endif
